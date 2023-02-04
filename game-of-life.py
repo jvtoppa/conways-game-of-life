45dd02f318c46c0847ea7b62f81d1235d7fe0582
João Vitor Toppa <53586039+jvtoppa@users.noreply.github.com>
2023-02-04 03:38:08 -0300

    v1.0 done - the game now works!

--- game-of-life.py
+++ game-of-life.py
@@ -19,13 +19,15 @@ def drawGrid():
 
 def cellGen():
     global mObj
-    xr = random.randint(1,21)
-    yr = random.randint(1,21)
+    xr = random.randint(0,20)
+    yr = random.randint(0,20)
     tp = (xr,yr)
-    if tp in map:
-        cell = pygame.Rect(tp[0]*20, tp[1]*20, 20, 20)
-        pygame.draw.rect(window, (0,0,0), cell)
-        mObj.append(cell)
+    #removes duplicates
+    if pygame.Rect(tp[0]*20, tp[1]*20, 20, 20) not in mObj:
+        if tp in map:
+            cell = pygame.Rect(tp[0]*20, tp[1]*20, 20, 20)
+            pygame.draw.rect(window, (0,0,0), cell)
+            mObj.append(cell)
 
 def matrixGen(cells):
     map_coords_bool = []
@@ -47,15 +49,21 @@ def main():
     window = pygame.display.set_mode((400, 400))
 
     window.fill((255, 255, 255))
-    drawGrid()
+    
     for w in range(0,50):
         cellGen()
 
     #Cria a matriz booleana com os valores das células
     #Update do display do pygame
     pygame.display.update()
+    
+    valCel = []
     #O jogo em si
     while True:
+        window.fill((255,255,255))
+        drawGrid()
+        for i in mObj:
+            pygame.draw.rect(window, (0,0,0), i)
         events = pygame.event.get()
         listM = matrixGen(mObj)
 
@@ -72,7 +80,7 @@ def main():
         array_tp_lista = array_transposto.tolist()
         positive_cell = numpy.where((array_transposto > 0))
         posl = positive_cell[0].tolist()
-        valCel = []
+        
 
         for i in range(0, len(posl)):
             val = (positive_cell[0][i], positive_cell[1][i])
@@ -108,33 +116,79 @@ def main():
                 adj.append((valCel[valo][0] - 1, valCel[valo][1] + 1))
 
             adj_matrix.append(adj)
-          #  print("Para", valCel[valo], ":")
-        #    print(adj)
 
         #VALCEL = TODOS OS QUADRADOS PINTADOS
         #ADJ_MATRIX = TODAS AS POSIÇÕES ADJACENTES AOS QUADRADOS PINTADOS
         #Quantas adjacências se interseccionam?
-        lst = []
+        adjacent = []
+        nonAdjacent = []
+        allAdj = []
+
         for i in range(0, len(adj_matrix)):
             for j in adj_matrix[i]:
-                lst.append(j)
-        c = Counter(lst)
-        #print(c)       
+                if j in valCel:
+                    adjacent.append(j)       
+        for e in valCel:
+            if e not in adjacent:
+                nonAdjacent.append(e)
+        for x in range(0, len(adj_matrix)):
+            for y in adj_matrix[x]:
+                allAdj.append(y)
 
-        newDict = dict()
-        
+        adj_matrix = []
+        c = Counter(adjacent)
+        c2 = Counter(allAdj)
+
+        #survival = dict()
+
+        newCells = dict()
+
+        deadCells = dict()
         #Todas as adjacencias
         for (key, value) in c.items():
-            if value >= 2:
-                newDict[key] = value
-        print(newDict)
+            if value >= 4:
+                deadCells[key] = value
 
-                    
-                    
-        #Framerate
-        time.sleep(3/1)
+        for (key, value) in c2.items():
+            if value == 3:
+                newCells[key] = value
 
+        for values in newCells.keys():
+            if (values[0],values[1]) not in valCel:
+                celula = pygame.Rect(values[1]*20, values[0]*20, 20, 20)
+                valCel.append((values[0],values[1]))
+                mObj.append(celula)   
+
+        #ESTA INVERTIDO EM mOBJ
+        #CASO EM QUE AS CELULAS NAO TEM ADJACENCIAS
+        for valores in nonAdjacent:
+
+            while (valores[0],valores[1]) in valCel:
+                valCel.remove((valores[0],valores[1]))
+
+            object = pygame.Rect(valores[1]*20, valores[0]*20, 20, 20)
+
+            if object in mObj:
+                mObj.remove(object)
+
+        #CASO EM QUE AS CELULAS TEM 4 OU MAIS ADJACENCIAS        
+        for valores in deadCells.keys():
+
+            if (valores[0],valores[1]) in valCel:
+                valCel.remove((valores[0],valores[1]))
+
+                object = pygame.Rect(valores[1]*20, valores[0]*20, 20, 20)
+
+                if object in mObj:
+                    mObj.remove(object)
+
+        valCel = []
         pygame.display.update()
+        
+        #Framerate
+        time.sleep(1/10)
+        
+        
 
         #Esc p/ fechar o programa
         for event in events:
